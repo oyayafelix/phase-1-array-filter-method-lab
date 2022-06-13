9bcf5b6984bbf2bed6be98ca56dc2d864784c84a
oyayafelix <oyayafelix@gmail.com>
2022-06-13 14:41:08 +0300

    commit tree 0e9a2484cf062ca7f93e54d1e447d1190e9aa3b8

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(names, searchName){
+    return names.filter(driver => searchName.toLowerCase() === driver.toLowerCase())
+  }
+  
+  function fuzzyMatch(names, searchString){
+    return names.filter(driver => driver.startsWith(searchString))
+  }
+  
+  function matchName(names, nameString){
+    return names.filter(driver => driver.name === nameString)
+  } 
